fb2480332d51b466bcb6ea821fa2c959d563cca1
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2023-03-13 21:14:49 +0600

    Navbar responsive done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi I am Restaurant</h1>
+      <Home/>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <Navbar />
     </div>
   );

bda349d11f70435bcd60d0c789fbea5f1216298f
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2023-03-13 23:48:26 +0600

    here section done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,33 @@
 import React from "react";
 import Navbar from "./Navbar";
+import BannerBGpng from "../assets/home-banner-background.png";
+import BannerPng from "../assets/home-banner-image.png";
+import { FiArrowRight } from "react-icons/fi";
 
 const Home = () => {
   return (
     <div className="home-container">
       <Navbar />
+      <div className="home-banner-container">
+        <div className="home-bannerImage-container">
+          <img src={BannerBGpng} alt="" />
+        </div>
+        <div className="home-text-section">
+          <h1 className="primary-heading">
+            Your Favourite Food Delivered Hot & Fresh
+          </h1>
+          <p className="primary-text">
+            Healthy switcher chefs do all the prep work, like peeding, chopping
+            & marinating, so you can cook a fresh food.
+          </p>
+          <button className="secondary-button">
+            Order Now <FiArrowRight />
+          </button>
+        </div>
+        <div className="home-image-section">
+          <img src={BannerPng} alt="" />
+        </div>
+      </div>
     </div>
   );
 };

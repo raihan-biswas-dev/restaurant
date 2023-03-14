5604f166db9d0b04a7b3b1a2c2160a65ef7eb3a2
raihan-biswas-dev <raihanbiswas.dev@gmail.com>
2023-03-14 22:06:42 +0600

    Testimonials section done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Home from "./components/Home";
 import About from "./components/About";
-import Work from './components/Work';
+import Work from "./components/Work";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
-      <Work/>
+      <Work />
+      <Testimonials />
     </div>
   );
 }
